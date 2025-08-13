b55b6ed2dd4f8315bc21071eccfb300eedf35386
prakhar-dv <prakharmishraspn91@gmail.com>
2025-08-13 18:53:26 +0530

    Update MainActivity.kt

--- android/app/src/main/kotlin/com/pricehawk/app /MainActivity.kt
+++ android/app/src/main/kotlin/com/pricehawk/app /MainActivity.kt
@@ -1,6 +1,6 @@
-package com.prakhar.pricehawk;
+package com.prakhar.pricehawk
 
-import io.flutter.embedding.android.FlutterActivity;
+import io.flutter.embedding.android.FlutterActivity
 
-public class MainActivity extends FlutterActivity {
+class MainActivity : FlutterActivity() {
 }
